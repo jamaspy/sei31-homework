a7db4dd0f79cb392f0497941222f70b54a507138
Thomas Hexton <tom.hexton@gmail.com>
2019-03-24 18:20:54 +1100

    almost got crossover working

--- tom_hexton/week-01/mta/js/main.js
+++ tom_hexton/week-01/mta/js/main.js
@@ -1,15 +1,15 @@
-console.log("MTA time, bitch.");
-console.log("--------------------");
+console.log( "MTA time, bitch." );
+console.log( "--------------------" );
 
 const mtaNetwork = {
-	lineN: ['8th', 'Union Station', '23rd', '28th', '34th', 'Times Square'],
-	lineL: ['1st', '3rd', 'Union Station', '6th', '8th'],
-	line6: ['Astor Place', 'Union Station', '23rd', '28th', '33rd', 'Grand Central'],
+	n: [ '8th', 'Union Station', '23rd', '28th', '34th', 'Times Square' ],
+	l: [ '1st', '3rd', 'Union Station', '6th', '8th' ],
+	six: [ 'Astor Place', 'Union Station', '23rd', '28th', '33rd', 'Grand Central' ],
 };
 
-const sameLineStationAddition = function(trainLine) {
+const sameLineStationAddition = function( trainLine ) {
 
-	console.log(`You'll get on at: ${ trainLine[ boardingIndex ] }.`);
+	console.log( `You'll get on at: ${ trainLine[ boardingIndex ] }.` );
 
 	traverseBoardingIndex = boardingIndex + 1;
 	traverseAlightingIndex = alightingIndex - 1;
@@ -17,126 +17,141 @@ const sameLineStationAddition = function(trainLine) {
 	// console.log(`traverseBoardingIndex: ${ traverseBoardingIndex }, traverseAlightingIndex: ${ traverseAlightingIndex }`);
 
 	// for loop that starts at the boardingIndex and proceeds is as long as alightingIndex
-	for (let i = traverseBoardingIndex; i <= traverseAlightingIndex; i += 1) {
-		console.log(`You'll traverse across: ${ trainLine[ i ] }`);
+	for ( let i = traverseBoardingIndex; i <= traverseAlightingIndex; i += 1 ) {
+		console.log( `You'll traverse across: ${ trainLine[ i ] }` );
 	}
 
-	console.log(`And get off at: ${ trainLine[ alightingIndex ] }`);
+	console.log( `And get off at: ${ trainLine[ alightingIndex ] }` );
 };
 
-const sameLineStationSubtraction = function(traiLine) {
+const sameLineStationSubtraction = function( trainLine ) {
 
-	console.log(`You'll get on at: ${ trainLine[ boardingIndex ] }`);
+	console.log( `You'll get on at: ${ trainLine[ boardingIndex ] }` );
 
 	traverseBoardingIndex = boardingIndex - 1;
 	traverseAlightingIndex = alightingIndex + 1;
 	// print current boarding and alighting values
 	// console.log(`traverseBoardingIndex: ${ traverseBoardingIndex }, traverseAlightingIndex: ${ traverseAlightingIndex }`);
 
-	for (let i = traverseBoardingIndex; i >= traverseAlightingIndex; i -= 1) {
-		console.log(`You'll traverse across: ${ trainLine[ i ] }`);
+	for ( let i = traverseBoardingIndex; i >= traverseAlightingIndex; i -= 1 ) {
+		console.log( `You'll traverse across: ${ trainLine[ i ] }` );
 	}
 
-	console.log(`And get off at: ${ trainLine[ alightingIndex ] }`);
+	console.log( `And get off at: ${ trainLine[ alightingIndex ] }` );
 };
 
-const planTrip = function(boardLine, boardStation, alightLine, alightStation) {
+const increasingArrayIndexPrintFuction = function( boardLine, boardStation ) {
+	// need to find index of boardStation
+	boardingIndex = mtaNetwork[boardLine].indexOf( boardStation );
+
+	traverseBoardingIndex = boardingIndex + 1;
+
+	for ( let i = traverseBoardingIndex; i <= mtaNetwork[boardLine].indexOf('Union Station'); i += 1 ) {
+		if (true) {
+			console.log( `You'll traverse across: ${ mtaNetwork[boardLine][ i ] }` );
+
+		}
+	}
+
+};
+
+const planTrip = function( boardLine, boardStation, alightLine, alightStation ) {
 
 	// turn inputs into capitals so don't need to catch both caps and non caps in each if case.
-	boardLine = boardLine.toUpperCase();
-	alightLine = alightLine.toUpperCase();
+	boardLine = boardLine.toLowerCase();
+	alightLine = alightLine.toLowerCase();
+
+	// converting input of '6' to 'six' for array referencing
+	if (boardLine === '6') {
+		boardLine === 'six';
+	};
 
 	// if both stops are on the N line
-	if (boardLine === 'N' && alightLine === 'N') {
+	if ( boardLine === 'n' && alightLine === 'n' ) {
 		// assign index1 to where they board the train
-		boardingIndex = mtaNetwork.lineN.indexOf(boardStation);
+		boardingIndex = mtaNetwork.n.indexOf( boardStation );
 
 		// assign index2 to where they alight the train
-		alightingIndex = mtaNetwork.lineN.indexOf(alightStation);
+		alightingIndex = mtaNetwork.n.indexOf( alightStation );
 
 		// assign to pass into sameLineStationAddition function
-		trainLine = mtaNetwork.lineN;
+		trainLine = mtaNetwork.n;
 
 		// if board[ i ] is less than than alight[ i ] then run this function
-		if (boardingIndex < alightingIndex) {
-			sameLineStationAddition(trainLine);
+		if ( boardingIndex < alightingIndex ) {
+			sameLineStationAddition( trainLine );
 		}
 
 		// if board[ i ] is greater than alight[ i ] then run this code
-		if (boardingIndex > alightingIndex) {
-			sameLineStationSubtraction(trainLine);
+		if ( boardingIndex > alightingIndex ) {
+			sameLineStationSubtraction( trainLine );
 		}
 	}
 
 	// if both stops are on the L line
-	if (boardLine === 'L' && alightLine === 'L') {
+	if ( boardLine === 'l' && alightLine === 'l' ) {
 		// assign index1 to where they board the train
-		boardingIndex = mtaNetwork.lineL.indexOf(boardStation);
+		boardingIndex = mtaNetwork.l.indexOf( boardStation );
 
 		// assign index 2 to where they alight the train
-		alightingIndex = mtaNetwork.lineL.indexOf(alightStation);
+		alightingIndex = mtaNetwork.l.indexOf( alightStation );
 
 		// assign to pass into sameLineStationAddition function
-		trainLine = mtaNetwork.lineL;
+		trainLine = mtaNetwork.l;
 
 		// if board[ i ] is less than than alight[ i ] then run this function
-		if (boardingIndex < alightingIndex) {
+		if ( boardingIndex < alightingIndex ) {
 			sameLineStationAddition();
 		}
 
 		// if board[ i ] is greater than alight[ i ] then run this code
-		if (boardingIndex > alightingIndex) {
+		if ( boardingIndex > alightingIndex ) {
 			sameLineStationSubtraction();
 		}
 	}
 
 	// if both stops are on the 6 line
-	if (boardLine === '6' && alightLine === '6') {
+	if ( boardLine === 'six' && alightLine === 'six' ) {
 		// assign index1 to where they board the train
-		boardingIndex = mtaNetwork.line6.indexOf(boardStation);
+		boardingIndex = mtaNetwork.six.indexOf( boardStation );
 
 		// assign index 2 to where they alight the train
-		alightingIndex = mtaNetwork.line6.indexOf(alightStation);
+		alightingIndex = mtaNetwork.six.indexOf( alightStation );
 
 		// assign to pass into sameLineStationAddition function
-		trainLine = mtaNetwork.line6;
+		trainLine = mtaNetwork.six;
 
 		// if board[ i ] is less than than alight[ i ] then run this function
-		if (boardingIndex < alightingIndex) {
+		if ( boardingIndex < alightingIndex ) {
 			sameLineStationAddition();
 		}
 
 		// if board[ i ] is greater than alight[ i ] then run this code
-		if (boardingIndex > alightingIndex) {
+		if ( boardingIndex > alightingIndex ) {
 			sameLineStationSubtraction();
 		}
 	}
 
-	// if someone gets on '8th' on lineN and wants to go to Grand Central on line6
-	// i need to calculate how many stops to Union Station on the first line, and print that out. and then print out the stops before hittin ghte index of the input alightStation on the relevant line.
+	// if board n and alight l OR board l and alight n
+	if ( boardLine === 'n' && alightLine === 'l' ) {
+		console.log(`You're getting on at ${mtaNetwork[boardLine].indexOf( boardStation )}.`);
+		if ( mtaNetwork[boardLine].indexOf( boardStation ) < mtaNetwork[boardLine].indexOf( 'Union Station' ) ) {
+			increasingArrayIndexPrintFuction( boardLine, boardStation );
+		} else if ( mtaNetwork.indexOf( boardStation ) > nUnionIndex ) {
+			decreasingArrayIndexPrintFunction( boardLine, boardStation );
+		}
+	}
 
-	// i can do this by findingout if the boardStation index is greater than or less than the Union Station index.
-	// depending on that outcome, I will use the additionPrintFuction or subtractionPrintFunction
-
-	// then find the index of Union Station on the new line and find the index of the final destination
-	// if the union station index is greater than alightStation index run the additionPrintFuction otherwise run the subtractionPrintFunction.
-
-	// the additionPrintFuction mu
-
-
-	// if the index of 'Union Station' is > boardStation then use the subtractionPrintFunction()
-
-	// if the index of 'Union Station' is < boardStation then use the additionPrintFunction()
-
-	if (boardLine === 'N' && alightLine === 'L') {
+	// if board n and alight six
+	if ( boardLine === 'N' && alightLine === '6' ) {
 
 	}
 
-	if (boardLine === 'N' && alightLine === '6') {
+	// if board l and alight six
+	if ( boardLine === 'L' && alightLine === '6' ) {
 
 	}
 
-	if (boardLine === 'L' && alightLine === '6') {
-
-	}
 };
+
+planTrip('N', '8th', 'L', '1st');
