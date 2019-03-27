6a5dc1f2f030b9ae45c5845c84210c9b550f618a
Thomas Hexton <tom.hexton@gmail.com>
2019-03-27 23:54:35 +1100

    got the cat strutting his stuff

--- tom_hexton/week-02/dom-manipulation/js/main.js
+++ tom_hexton/week-02/dom-manipulation/js/main.js
@@ -8,36 +8,39 @@
 //
 // Bonus #3: When the cat reaches the middle of the screen, replace the img with an image of a cat dancing, keep it dancing for 10 seconds, and then replace the img with the original image and have it continue the walk.
 
+// select and save only IMG in DOM
 const image = document.getElementsByTagName( 'img' )[ 0 ];
+// set image to absolute left.
 image.style.position = 'absolute';
 image.style.left = 0;
 
-let newLeft = 0;
-let maxRight = 100;
-// let walkRight;
+// define my left counter to start with
+let leftEdge = 0;
+// set max width to screen - cat image
+let rightEdge = window.innerWidth - 296;
+console.log(window.innerWidth);
 
+// first function to walk cat right
 const catWalk = function() {
-	newLeft = newLeft + 1;
-	image.style.left = newLeft + "%";
-	if ( parseInt( image.style.left ) >= maxRight ) {
+	leftEdge = leftEdge + 10;
+	image.style.left = leftEdge + "px";
+	if ( parseInt( image.style.left ) >= rightEdge ) {
 		image.style.transform = "scaleX(-1)";
 		clearInterval( beginWalk );
-		beginWalk = window.setInterval( catWalkLeft, 10 );
+		beginWalk = window.setInterval( catWalkLeft, 50 );
 	};
 };
 
 const catWalkLeft = function() {
-	newLeft = newLeft - 1;
-	image.style.left = newLeft + '%';
+	leftEdge = leftEdge - 10;
+	image.style.left = leftEdge + 'px';
 	if ( parseInt( image.style.left ) === 0 ) {
 		image.style.transform = "scaleX(1)";
 		clearInterval( beginWalk );
-		// image.style.left = 0;
-		// console.log(walkRight);
-		beginWalk = window.setInterval( catWalk, 10 );
+		beginWalk = window.setInterval( catWalk, 50 );
 	};
 };
 
 ////////////////////////////////////////////////////////////////////////////////
-let beginWalk = window.setInterval( catWalk, 10 );
+let beginWalk = window.setInterval( catWalk, 50 );
 ////////////////////////////////////////////////////////////////////////////////
