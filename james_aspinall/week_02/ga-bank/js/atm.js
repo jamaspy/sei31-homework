d6c3ae176ac11cff392f172e9bf3335d248f161f
James Aspinall <jamesaspinall@Jamess-MacBook.lan>
2019-03-29 19:18:52 +1100

    jQuery Practice & Moving Cat

---
+++ james_aspinall/week_02/ga-bank/js/atm.js
@@ -0,0 +1,13 @@
+const savingsBalance = [];
+const checkingBalance = [];
+$(document).ready(function(){
+
+let displayedBalance = $("#checking-balance").html();
+console.log(displayedBalance);
+savingsBalance.push(displayedBalance);
+console.log(savingsBalance);
+
+
+
+
+});
