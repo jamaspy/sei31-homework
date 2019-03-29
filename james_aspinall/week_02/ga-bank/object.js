5cda7cc036f7e1d89b6151025d2ac3118d9fecd5
jamaspy <aspin@me.com>
2019-03-30 08:45:58 +1100

    GA Bank homework so far from **iMAC**

---
+++ james_aspinall/week_02/ga-bank/object.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+console.log("WORKING");
+// const theBank = {
+//   checkingAccount: $("#checking-balance").html()
+//   savingsAccount: $("#savings-balance").html(),
+//
+//   depositAmount:,
+//   depositInChecking:,
+//   withdrawChecking:,
+//
+//   savingsAmount:,
+//   depositInSavings:,
+//   withdrawSavings:,
+//
+// };
+}
