cdaf37244cb1b80d0816e3b907affa3ad6f983e0
Adrian Thorburn <adrian.thorburn@gmail.com>
2019-03-30 17:42:18 +1100

    100% ATM - but needs DRYifying

--- adrian_thorburn/week02/ga-bank/js/atm.js
+++ adrian_thorburn/week02/ga-bank/js/atm.js
@@ -6,54 +6,96 @@ const accounts = {
   savings: 0,
 }
 
+// acount at zero zeroChecker
+//chk
+const zeroCheckerChk = function () {
+  if (accounts.checking === 0) {
+  $('#checking').addClass('zero');
+} else {
+  $('#checking').removeClass('zero');
+  }
+}
+
+//sav
+const zeroCheckerSav = function () {
+  if (accounts.savings === 0) {
+  $('#savings').addClass('zero');
+} else {
+  $('#savings').removeClass('zero');
+  }
+}
+
+
 ////////// Bank processes //////////
 const chkDeposit = function (value){
   accounts.checking += value
   $('#checking-balance').text(`$ ${accounts.checking}`) // update display of new ammount
+  zeroCheckerChk();
 }
 
-$('#checking-deposit').on('click', function(event){
-  const $chkDepositAmount = parseInt($('#checking-amount').val()); //
-  chkDeposit($chkDepositAmount);
-  //$('#checking-amount').val('')
-});
-
-const chkWithdraw = function (value){
-  accounts.checking -= value
+const chkWithdraw = function (value) {
+  if (value > accounts.checking){
+    negative = accounts.checking - value
+    accounts.checking = 0
+    accounts.savings += negative
+  } else {
+    accounts.checking -= value
+  }
   $('#checking-balance').text(`$ ${accounts.checking}`)
+  $('#savings-balance').text(`$ ${accounts.savings}`)
+  zeroCheckerChk();
 }
+
+const savDeposit = function (value){
+  accounts.savings += value
+  $('#savings-balance').text(`$ ${accounts.savings}`)
+  zeroCheckerSav();
+}
+
+const savWithdraw = function (value){
+  if (value > accounts.savings){
+    negative = accounts.savings - value
+    accounts.savings = 0
+    accounts.checking += negative
+  } else {
+    accounts.savings -= value
+  }
+  $('#checking-balance').text(`$ ${accounts.checking}`)
+  $('#savings-balance').text(`$ ${accounts.savings}`)
+  zeroCheckerSav();
+}
+
+
+
+//jquery visual updaters
+$('#checking-deposit').on('click', function(event){
+  const $depositAmount = parseInt($('#checking-amount').val()); //
+  chkDeposit($depositAmount);
+  // $('#checking-amount').val('')
+});
 
 $('#checking-withdraw').on('click', function(event){
-  const $chkWithdrawAmount = parseInt($('#checking-amount').val()); //
-  chkWithdraw($chkWithdrawAmount);
-  //$('#checking-amount').val('')
+  const $withdrawAmount = parseInt($('#checking-amount').val()); //
+  if ($withdrawAmount > accounts.savings + accounts.checking) {
+    console.log('not enough cash')
+  } else {
+  chkWithdraw($withdrawAmount);
+  // $('#checking-amount').val('')
+  }
 });
-
-
-const savDeposite = function (value){
-  accounts.savings += value
-  $('#savings.balance').text(`$ ${accounts.savings}`)
-}
 
 $('#savings-deposit').on('click', function(event){
   const $depositAmount = parseInt($('#savings-amount').val()); //
-  savDeposite($depositAmount);
+  savDeposit($depositAmount);
   //$('#checking-amount').val('')
 });
 
-const savWithdraw = function (value){
-  accounts.savings -= value
-  $('#savings.balance').text(`$ ${accounts.savings}`)
+$('#savings-withdraw').on('click', function(event){
+  const $withdrawAmount = parseInt($('#savings-amount').val()); //
+  if ($withdrawAmount > accounts.savings + accounts.checking) {
+    console.log('not enough cash')
+  } else {
+  savWithdraw($withdrawAmount);
+  //$('#checking-amount').val('')
 }
-
-// $('#savings-withdraw').on('click', function(event){
-//   const $withdrawAmount = parseInt($('#savings-amount').val()); //
-//   savWithdraw($withdrawAmount);
-//   //$('#checking-amount').val('')
-// });
-
-// $('#checking-deposit').on('click', function(event){
-//   const $depositeAmmount = parseInt($('#checking-amount').val()); //
-//   chkDeposit($depositeAmmount);
-//   $('#checking-amount').val('')
-// });
+});
