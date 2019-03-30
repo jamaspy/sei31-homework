05350b588ba6767168ed740568668299135273cd
Thomas Hexton <tom.hexton@gmail.com>
2019-03-30 19:56:16 +1100

    progress update

--- tom_hexton/week-02/ga-bank/js/atm.js
+++ tom_hexton/week-02/ga-bank/js/atm.js
@@ -2,47 +2,98 @@ $( document ).ready( function() {
 
 	// create a jQuery object for the checking account DOM node
 	const $checkingBalance = $( '#checking-balance' );
+	// select Checking input
+	const $checkingInput = $( '#checking-amount' );
 
 	// create a jQuery object for the savings account DOM node
 	const $savingsBalance = $( '#savings-balance' );
-	// get the current innerHTML
-	let currentSavings = $savingsBalance.html();
-	// remove first character ($) and convert to number
-	let currentSavingsNumber = Number(currentSavings.slice(1));
-	console.log( 'Savings Balance: $' + currentSavingsNumber, typeof currentSavingsNumber ); // debugging ONLY
+	// select Savings input
+	const $savingsInput = $( '#savings-amount' );
 
+	////////////////////////////////////////////////////////////////////////////
+	// button events
 
-	const $checkingInput = $( '#checking-amount' );
-	// select checking deposit button & add on click listener
-	$( '#checking-deposit' ).on( 'click', function() {
-
-		// get the current innerHTML
-		let currentChecking = $checkingBalance.html();
+	const getBalance = function( accountBalance ) {
+		// get balance's current innerHTML
 		// remove the dollar sign from returned string and convert to number
-		let currentCheckingNumber = Number(currentChecking.slice(1));
+		return Number( accountBalance.html().slice( 1 ) );
+	};
 
-		console.log( 'Checking Balance: $' + currentCheckingNumber, typeof currentCheckingNumber); // debugging ONLY
+	const getUserInput = function( accountInput ) {
+		// assign the value from chosen account and convert to number
+		return Number( accountInput.val() );
+	};
 
-		// assign the value from checking input
-		let checkingValue = Number($checkingInput.val());
-		// add that value to current checking balance amount
-		console.log(typeof checkingValue);
-		$checkingBalance.html( `$${currentCheckingNumber} + ${checkingValue}` );
+	// Checking deposit ////////////////////
+	// select Checking deposit button & add on click listener
+	$( '#checking-deposit' ).on( 'click', function() {
+		// get current Checking balance and add Users input value
+		checkingTotal += getUserInput( $checkingInput );
+		$checkingBalance.html( "$" + checkingTotal );
+	} );
+
+	// Checking withdraw ////////////////////
+	// select Checking withdraw button & add on click listener
+	$( '#checking-withdraw' ).on( 'click', function() {
+		// get current Checking balance and subtract User input value
+		if ( checkingTotal - getUserInput( $checkingInput ) < 0 ) {
+			console.log( 'Sorry, not enough funds to make that withdrawl.' );
+		} else {
+			checkingTotal -= getUserInput( $checkingInput )
+			$checkingBalance.html( "$" + checkingTotal );
+		}
+	} );
+
+	// Savings deposit ////////////////////
+	// select Savings desposit button & add on click listener
+	$( '#savings-deposit' ).on( 'click', function() {
+		// get current Savings balance and add Users input value
+		savingsTotal += getUserInput( $savingsInput );
+		$savingsBalance.html( "$" + savingsTotal );
+
+		if ( savingsTotal > 0 ) {
+			console.log('savingsTotal > 0.');
+			$( '#savings' ).removeClass( 'zero' )
+		};
+	} );
+
+	// Savings withdraw ////////////////////
+	// select Savings desposit button & add on click listener
+	$( '#savings-withdraw' ).on( 'click', function() {
+		// get current Checking balance and subtract User input value
+		if ( savingsTotal - getUserInput( $savingsInput ) < 0 ) {
+			console.log( 'Sorry, not enough funds to make that withdrawl.' );
+		} else {
+			savingsTotal -= getUserInput( $savingsInput )
+			$savingsBalance.html( "$" + savingsTotal );
+
+			if (savingsTotal <= 0 ) {
+				console.log('savingsTotal <= 0.');
+				$( '#savings' ).addClass( 'zero' );
+			};
+		};
 
 	} );
 
-	// select savings desposit button & add on click listener
-	$( '#savings-deposit' ).on( 'click', function() {
+	////////////////////////////////////////////////////////////////////////////]
+	// on change BG colors
 
-		// get current innerHTML
-		let currentSavings = $( 'savings-balance' );
-		// remove the dollar sign from returned string and convert to number
-	});
+	// $( '#checking' ).on( 'change', function() {
+	// 	if ( checkingTotal > 0 ) {
+	// 		$( '#checking' ).addClass( 'zero' );
+	// 	} else {
+	// 		$( '#checking' ).removeClass( 'zero' )
+	// 	}
+	// } );
+
+	////////////////////////////////////////////////////////////////////////////
+	// totals
+
+	// Checking account balance
+	let checkingTotal = getBalance( $checkingBalance );
+
+	// Savings account balance
+	let savingsTotal = getBalance( $savingsBalance );
 
 	// debugger
 } );
-
-
-
-
-// select input box for checking account and assign input
---
+++ tom_hexton/week-02/ga-bank/js/totals.js
@@ -0,0 +1,2 @@
+// var checkingTotal = Number( $( '#checking-balance' ).html().slice( 1 ) );
+// var savingsTotal = Number( $( '#savings-balance' ).html().slice( 1 ) );
