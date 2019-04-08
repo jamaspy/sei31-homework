2b6be212db2275f215ecf3ab2cfe66b8873ef31a
edgarjin88 <edgar.jin88@gmail.com>
2019-04-08 22:13:08 +1000

    GA bank Joel's solution added

--- yongduck_jin/week02/day10/ga-bank/js/atm.js
+++ yongduck_jin/week02/day10/ga-bank/js/atm.js
@@ -1,85 +1,63 @@
-//overdraft, business logic. 
-// need to seperate business logic off the DOM. 
-//overdraft side, has to be jQuery free. 
-// also, later import this faile later. 
-// .val('').focus(); empty value. 
+// This code knows nothing of the rules for deposits or withdrawals.
+// Its job is to connect event handlers (and user interaction) with the
+// corresponding operations in accounts.js.
 
-// const render = function(){  //render function to add up everything
-//   $('#checkging-balance').text('$' + accounts.checkingBalance);
-//   $('#saveings-balance').text('$' + accounts.savingsBalance);
-//  $('.zero').removeClass('zero'); / remove all 'zero class' first 
-  // if(accounts.savingsBalanc <0){
-  //   $('savings-balanc').addClass('zero') //// add 'zero' class of < 0
-  // }
-// }
-//this 'render' funtion has to be in the beginning of code. 
-// advantage of this render code is we can always change for the promotion or any event, without touching DOM. 
+// It also provides a much-used render() function which updates all
+// aspects of the page with the precise current balances and zero
+// styling, all in one place:
 
+const render = function () {
+  $('#checking-balance').text('$' + accounts.checkingBalance);
+  $('#savings-balance').text('$' + accounts.savingsBalance);
+//updating machine
 
-//call the render function every time when data changed.
-// seperate the function, function by function. 
+  $('.zero').removeClass('zero');
 
-//// Preferred way below. Seperated logic. 
-//// check  the function every step if works or not. 
+  if (accounts.checkingBalance <= 0) {
+    $('#checking-balance').addClass('zero');
+  }
 
-
-const accounts = {
-  checkingBalance : 0, 
-  savingsBalance: 0, 
-  total: function(){
-    return this.checkingBalance + this.savingsBalance; 
-  } // total has to be a function. Add the negative method as well into here. 
-}
+  if (accounts.savingsBalance <= 0) {
+    $('#savings-balance').addClass('zero');
+  }
+};
 
 
 
-// ////////////// logic integrated. The way to avoid. From below. 
+// All the event handlers must wait for the DOM to be ready before
+// they can be attached.
+$(document).ready(function () {
+  render(); // Update the page with balances and zeros ASAP.
 
-// $(document).ready(function(){
+  $('#checking-deposit').on('click', function () {
+    const amount = $('#checking-amount').val();
+    $('#checking-amount').val('').focus();
+    accounts.checkingDeposit(amount);
+    render();
+  });
 
-//  $('#checking-deposit').on('click', function(){
+  $('#checking-withdraw').on('click', function () {
+    const amount = $('#checking-amount').val();
+    $('#checking-amount').val('').focus();
+    accounts.checkingWithdraw(amount);
+    render();
+  });
 
-//   const amount=$('#checking-amount').val()
-//   const currentBalance = $('#checking-balance').text().slice(1)
+  //클릭을 했을 때마다 어떤 과정들이 순차적으로 벌어지는지를 생각해 보라. 
+  //독립적인 각각의 펑션들. 
+  //그리고 그 펑션들은 옵젝트 안에 있다. 
 
+  $('#savings-deposit').on('click', function () {
+    const amount = $('#savings-amount').val();
+    $('#savings-amount').val('').focus();
+    accounts.savingsDeposit(amount);
+    render();
+  });
 
-//   // $('checking-amount').val('').focus();
-
-//   const newBalance = amount + currentBalance;
-//   console.log(newBalance);
-//   $('#checking-balance').text('$'+ newBalance)
-//  })
-
-//  $('#checking-withdraw').on('click', function(){
-//    const amount= + $('#checking-amount').val();
-//    const currentBalance = $('#checking-balance').text().slice(1)
-//    const newBalance = currentBalance - amount; 
-
-//    if(newBalance >=0){
-//      $('#checking-balance').text('$'+newBalance)
-//    }
-//  })
-
-
-//  $('#savings-deposit').on('click', function(){
-
-//   const amount=$('#savings-amount').val()
-//   const currentBalance = $('#savings-balance').text().slice(1)
-
-//   const newBalance = amount + currentBalance;
-//   console.log(newBalance);
-//   $('#savings-balance').text('$'+ newBalance)
-//  })
-
-//  $('#savings-withdraw').on('click', function(){
-//    const amount= + $('#savings-amount').val();
-//    const currentBalance = $('#savings-balance').text().slice(1)
-//    const newBalance = currentBalance - amount; 
-
-//    if(newBalance >=0){
-//      $('#savings-balance').text('$'+newBalance)
-//    }
-//  })
-// })
-
-/////////
+  $('#savings-withdraw').on('click', function () {
+    const amount = $('#savings-amount').val();
+    $('#savings-amount').val('').focus();
+    accounts.savingsWithdraw(amount);
+    render();
+  });
+});
