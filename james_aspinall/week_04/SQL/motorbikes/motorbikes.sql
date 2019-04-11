a7441746670eaf855ee5e0aadb9ceb07f07a4b1d
jamaspy <aspin@me.com>
2019-04-11 21:51:57 +1000

    SQL Database Homework _ Motorbikes

---
+++ james_aspinall/week_04/SQL/motorbikes/motorbikes.sql
@@ -0,0 +1,7 @@
+CREATE TABLE motorbikes (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  model TEXT,
+  make TEXT,
+  image TEXT
+);
+-- INSERT INTO motorbikes (model, make) VALUES ("Ninja250", "Kawasaki");
